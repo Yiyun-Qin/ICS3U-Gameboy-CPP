f9020a752624782ad756ed58ba7e825dd0ead208
Yiyun Qin <yiyun.qin@mths.ca>
2022-05-26 18:57:35 +0000

    sprite

--- game.c
+++ game.c
@@ -2,11 +2,20 @@
 //
 // Created by: Yiyun Qin
 // Created on: May 2022
-// This program is the "Hello, World!" program for the GameBoy
+// This program is the sprite program for the GameBoy
 
 #include <stdio.h>
 #include <gb/gb.h>
 
+#include <smiler.h>
+
+
 int main() {
-	printf("Hello, World!");
+    // start from nth tiles, to load how many elements, file name 
+	set_sprite_data(0, 1, TileData);
+	// number of sprite, the nth tile(smile)
+	set_sprite_tile(0, 0);
+	// move the nth sprite to (88, 78)
+	move_sprite(0, 88, 78);
+	SHOW_SPRITES;
 }
