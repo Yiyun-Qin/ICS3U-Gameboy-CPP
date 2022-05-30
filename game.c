343ca07a27b7286a02ccdf56a680a999c165db8e
Yiyun Qin <yiyun.qin@mths.ca>
2022-05-30 18:49:35 +0000

    first part move

--- game.c
+++ game.c
@@ -11,11 +11,20 @@
 
 
 int main() {
-    // start from nth tiles, to load how many elements, file name 
-	set_sprite_data(0, 1, TileData);
-	// number of sprite, the nth tile(smile)
-	set_sprite_tile(0, 0);
-	// move the nth sprite to (88, 78)
-	move_sprite(0, 88, 78);
-	SHOW_SPRITES;
+    // start from nth tiles, to load how many elements, file name
+    set_sprite_data(0, 1, TileData);
+    // number of sprite, the nth tile(smile)
+    set_sprite_tile(0, 0);
+    // move the nth sprite to (88, 78)
+    move_sprite(0, 88, 78);
+    SHOW_SPRITES;
+    
+    while (1) {
+        switch(joypad()) {
+            case J_LEFT:
+            // index of sprite to access, x, y
+                scroll_sprite(0, -10, 0);
+            break;
+        }
+    }
 }
