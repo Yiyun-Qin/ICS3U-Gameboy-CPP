d247119a7a5883ef102943cd8fa637092c3b6208
Yiyun Qin <yiyun.qin@mths.ca>
2022-06-05 21:05:49 +0000

    move

--- game.c
+++ game.c
@@ -18,13 +18,23 @@ int main() {
     // move the nth sprite to (88, 78)
     move_sprite(0, 88, 78);
     SHOW_SPRITES;
-    
+
     while (1) {
-        switch(joypad()) {
+        switch (joypad()) {;
             case J_LEFT:
             // index of sprite to access, x, y
-                scroll_sprite(0, -10, 0);
+                    scroll_sprite(0, -10, 0);
+            break;
+            case J_RIGHT:
+                scroll_sprite(0, 10, 0);
+            break;
+            case J_UP:
+                scroll_sprite(0, 0, -10);
+            break;
+            case J_DOWN:
+                scroll_sprite(0, 0, 10);
             break;
         }
+        delay(100);
     }
 }
