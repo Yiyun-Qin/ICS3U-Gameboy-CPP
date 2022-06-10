d9d627d88510c85d0df77f5b6d5090aac583a9dc
Yiyun Qin <yiyun.qin@mths.ca>
2022-06-10 02:41:12 +0000

    strange error

--- game.c
+++ game.c
@@ -6,27 +6,48 @@
 
 #include <stdio.h>
 #include <gb/gb.h>
+#include <gb/font.h>
 
 #include <smiler.h>
 #include <simpleBackground.c>
 #include <simpleBackgroundMap.c>
+#include <windowmap.c>
 
 
-int main() {
-    // x and y coordinates
+void main() {
+    // sprite x and y coordinates
     int x = 88;
     int y = 78;
     const screenX = 160;
     const screenY = 144;
     const spriteX = 8;
-    const spriteY = 8;
+    const spriteY = 8
+
+    // font
+    font_t min_font;
+
+    // initial and load font library
+    font_init();
+    min_font = font_load(font_min);  // 36 tiles
+    font_set(min_font);
+
+    // fonts, width and height
+    set_win_tiles(0, 0, 5, 1, windowmap)
+    // window layer is above other layers, so it overlap other layers
+    move_win(7, 120)
 
     // background
-    set_bkg_data(0, 7, backgroundtiles);
+    // start at 37 to prevent overlap with font
+    set_bkg_data(37, 7, backgroundtiles);
     // start at 0 in 0, x, y, go to backgroundmap to know how to use
     set_bkg_tiles(0, 0, 40, 18, backgroundmap);
+
+    // show the map
     SHOW_BKG;
+    SHOW_WIN;
     DISPLAY_ON;
+
+    // load sprite
     // start from nth tiles, to load how many elements, file name
     set_sprite_data(0, 1, TileData);
     // number of sprite, the nth tile(smile)
